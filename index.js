85a18228107bab1c9d5197aba834bbacc8eda994
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-01-19 16:10:43 +0300

    fs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs=require('fs');
+const path = require('path');
 const employees=[];
 employees.push({
     name:'hamesi',
@@ -12,7 +14,9 @@ const amos={
     sallary:1000
 }
 employees.push(amos);
-console.log(JSON.stringify(employees,null,3));
+// console.log(JSON.stringify(employees,null,3));
 const json=JSON.stringify(employees,null,3);
-const employees2=JSON.parse(json);
+// const employees2=JSON.parse(json);
 console.log(employees2);
+
+fs.writeFileSync(path.join(__dirname,'employees.json'),json)
